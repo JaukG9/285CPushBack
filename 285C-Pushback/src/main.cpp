bb4cdb7ea44313cdbd305515ec3081e2c2982bf5
JaukG9 <ayaangoswami17@gmail.com>
2025-10-24 17:18:37 -0500

    delete everything...

--- 285C-Pushback/src/main.cpp
+++ 285C-Pushback/src/main.cpp
@@ -1,94 +1,89 @@
-#include "main.h"
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       ayaan                                                     */
+/*    Created:      10/19/2025, 3:26:09 PM                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+using namespace vex;
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+controller Controller = controller();
+int axisLS, axisLT, axisRS, axisRT;
+
+motor Motor1 = motor(PORT1, ratio6_1, false);
+motor Motor2 = motor(PORT2, ratio6_1, false);
+motor Motor3 = motor(PORT3, ratio6_1, false);
+motor Motor4 = motor(PORT11, ratio6_1, true);
+motor Motor5 = motor(PORT12, ratio6_1, true);
+motor Motor6 = motor(PORT13, ratio6_1, true);
+motor_group leftMotors = motor_group(Motor1, Motor2, Motor3);
+motor_group rightMotors = motor_group(Motor4, Motor5, Motor6);
+
+motor Intake = motor(PORT20, false);
+bool reverseDrive = false;
+
+drivetrain DriveTrain = drivetrain(leftMotors, rightMotors, 260, 285, 252, mm, 48/36);
+
+// shoulder buttons (intake & conveyer)
+void r1Pressed(){
+    Intake.spin(forward, 200, rpm);
+}
+void r1Released(){
+    Intake.stop();
+}
+void r2Pressed(){
+    Intake.spin(reverse, 200, rpm);
+}
+void r2Released(){
+    Intake.stop();
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+void l2Pressed(){
+    reverseDrive = !reverseDrive;
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+// joysticks (tank drive)
+/* maybe inlcude gears to make rpm less erratic */
+void axisLTChanged(){
+    leftMotors.spin(reverseDrive ? reverse : forward, 6 * Controller.Axis3.position(), rpm);
+}
+void axisRTChanged(){
+    rightMotors.spin(reverseDrive ? reverse : forward, 6 * Controller.Axis2.position(), rpm);
+}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
+/*
+void axisLSChanged(){
+    leftDrive(axisLS);
+}
+void axisRSChanged(){
+    leftDrive(axisRS);
+}*/
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+// main
+int main() {
+    while(1) {
+        //print intake state to screen
+        Brain.Screen.clearScreen();
+        Brain.Screen.setCursor(10, 50);
+        Brain.Screen.print(Intake.isSpinning());
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+        //input (shoulders + joysticks)
+        Controller.ButtonR1.pressed(r1Pressed);
+        Controller.ButtonR1.released(r1Released);
+        Controller.ButtonR2.pressed(r2Pressed);
+        Controller.ButtonR2.released(r2Released);
+        Controller.ButtonL2.pressed(l2Pressed);
 
+        Controller.Axis3.changed(axisLTChanged);
+        Controller.Axis2.changed(axisRTChanged);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+        //don't run too many actions
+        this_thread::sleep_for(10);
+    }
 }
