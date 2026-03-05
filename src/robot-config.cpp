4e33f063d953c4849bed5554b60aacfe9ceecb2a
Ayaan Goswami <ayaangoswami17@gmail.com>
2026-03-05 17:50:48 -0600

    fixed some errors; compilation works now

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,13 +7,13 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 /* initialization of motors & pistons */
 pros::Motor conveyer1(-1, pros::MotorGearset::blue);
 pros::Motor conveyer2(10, pros::MotorGearset::blue);
-pros::ADIDigitalOut scraper('H'); // extends scraper piston
-pros::ADIDigitalOut trapdoor('G'); // extends midtake piston
-pros::ADIDigitalOut wing('F'); // extends wing
-pros::ADIDigitalOut pto('E'); // extends pto
-pros::ADIDigitalOut intakeFunnel('D'); //extends intakeFunnel
-pros::ADIDigitalOut doublePark('C'); // extends double park
-pros::ADIDigitalOut odomLift('B'); // odom lift
+pros::adi::DigitalOut scraper('H'); // extends scraper piston
+pros::adi::DigitalOut trapdoor('G'); // extends midtake piston
+pros::adi::DigitalOut wing('F'); // extends wing
+pros::adi::DigitalOut pto('E'); // extends pto
+pros::adi::DigitalOut intakeFunnel('D'); //extends intakeFunnel
+pros::adi::DigitalOut doublePark('C'); // extends double park
+pros::adi::DigitalOut odomLift('B'); // odom lift
 
 bool scraperActivated = false;
 bool trapdoorActivated = false;
