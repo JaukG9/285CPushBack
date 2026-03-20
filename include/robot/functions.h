f1658c211b4e91f05472584585b3113e9318355a
JaukG9 <ayaangoswami17@gmail.com>
2026-03-20 16:07:38 -0500

    commit

--- include/robot/functions.h
+++ include/robot/functions.h
@@ -13,3 +13,18 @@ void ptoChange();
 void doubleParkChange();
 void odomLiftChange();
 void tank();
+
+struct ChainToPointParams {
+    /** whether the robot should move forwards or backwards. True by default */
+    bool forwards = true;
+    /** the maximum speed the robot can travel at. Value between 0-127. 127 by default */
+    float maxSpeed = 127;
+};
+void chainToPoint(float x, float y, int timeout, ChainToPointParams params = {}, bool async = true);
+struct ChainToHeadingParams{
+    /** the direction the robot should turn in. AUTO by default */
+    AngularDirection direction = AngularDirection::AUTO;
+    /** the maximum speed the robot can turn at. Value between 0-127. 127 by default */
+    int maxSpeed = 127;
+};
+void chainToHeading(float theta, int timeout, ChainToHeadingParams params = {}, bool async = true);
